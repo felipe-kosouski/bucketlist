c6707e4dfe168912be38add86fc68c3aae008c92
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-04-09 20:08:55 -0300

    Added attribute name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
 
   protected
@@ -8,5 +9,10 @@ class ApplicationController < ActionController::Base
     return "layouts/session" if devise_controller?
 
     "layouts/application"
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    name 'Felipe'
     sequence (:email) { |n| "user#{n}@bucketlist.br" }
     password 'password'
     password_confirmation 'password'
