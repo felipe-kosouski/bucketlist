fb4a7e19febb8379a91130fd99b3b6a8547907a7
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-04-10 19:42:36 -0300

    Added users and admin areas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :admins do
+    get 'dashboard/index'
+  end
+
+  devise_for :admins
+  namespace :users do
+    get 'dashboard/index'
+  end
+
   root to: 'home#index'
+
+  devise_for :users
+
+  authenticate :user do
+    namespace :users do
+      root to: 'dashboard#index'
+    end
+  end
+
+  authenticate :admin do
+    namespace :admins do
+      root to: 'dashboard#index'
+    end
+  end
 end
---
+++ spec/factories/admins.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :admins do
+    
+  end
+end
